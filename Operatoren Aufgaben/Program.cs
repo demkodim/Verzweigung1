0e80248db4896ead623c41e98f307c5dc7422532
demko <demko@DESKTOP-SUT4EJA>
2022-09-07 20:14:34 +0200

    Добавьте файлы проекта.

---
+++ Operatoren Aufgaben/Program.cs
@@ -0,0 +1,61 @@
+﻿//Aufgabe 1
+/*
+Console.WriteLine("Bitte geben sie die erste Zahl ein");
+var zahl1 = Console.ReadLine();
+short short_Number1 = Convert.ToInt16(zahl1);
+
+Console.WriteLine("Bitte geben sie die zweite Zahl ein");
+var zahl2 = Console.ReadLine();
+short short_Number2 = Convert.ToInt16(zahl2);
+
+
+Console.WriteLine($"Die kleinste Zahl lautet: {Math.Min(short_Number2, short_Number1)}");
+*/
+
+
+
+
+//Aufgabe 2
+/*
+Console.WriteLine("RATTESPEIL");
+Console.WriteLine("Geben sie eine Zahl zwischen 0 und 1000 ein: ");
+ Random RandomNumber = new Random();
+
+ int random1 = RandomNumber.Next(0,1001 );
+
+ bool j = true;
+
+
+while (j)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    
+
+                    int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+                    if (yourNumber < random1)
+                    {
+                        Console.WriteLine("Die gesuchte Zahl ist größer.");
+                        break;
+                    }
+                    else if (yourNumber > random1)
+                    {
+                        Console.WriteLine("Die gesuchte Zahl ist kleiner.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du hast die Zahl eratten!");
+                        j = false;
+                    }
+
+                    Console.ReadLine();
+                    break;
+                }
+            }
+  */      
+
+//Aufgabe 3
+
+    
